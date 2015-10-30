38e1b3806aaaa4d6716b026c6c1a16c4c2db7620
Vincent Lepot <vincent.lepot@canaltp.fr>
2015-10-30 12:56:49 +0100

    Add HDP sandbox in hosts in container

--- refreshenv.sh
+++ refreshenv.sh
@@ -1,4 +1,4 @@
 docker-compose run --rm worker /bin/bash -c "cd /srv/stat-logger ; virtualenv venv"
 docker-compose run --rm worker /bin/bash -c "cd /srv/stat-logger ; source venv/bin/activate ; pip install -r requirements.txt"
 docker-compose run --rm worker /bin/bash -c "cd /srv/stat-logger ; source venv/bin/activate ; protoc -Inavitia-proto --python_out=stat_logger navitia-proto/*.proto"
-docker-compose run --rm worker /bin/bash -c "cd /srv/stat-logger ; source venv/bin/activate ; python stat_logger.py stat_logger.conf.yml"
+docker-compose run --rm worker /bin/bash -c "cd /srv/stat-logger ; source venv/bin/activate ; echo '172.17.42.1     sandbox.hortonworks.com' >> /etc/hosts ; python stat_logger.py stat_logger.conf.yml"
